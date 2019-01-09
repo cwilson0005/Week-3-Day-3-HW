07c58566397eecb2ffdabefca18bcaca45c62d50
Conor <cwilson0005@hotmail.co.uk>
2019-01-09 13:52:05 +0000

    Added intialize method to artist class

---
+++ models/artist.rb
@@ -0,0 +1,15 @@
+require('pg')
+require_relative('../db/sql_runner')
+require_relative('./album')
+
+class Artist
+
+  attr_reader(:id)
+  attr_accessor(:name)
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+  end
+
+end
